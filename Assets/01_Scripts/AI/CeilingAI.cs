eef2d60efb7d0aee85131c68a471cfc5bf8585ae
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2026-03-04 10:35:12 +0100

    Fix AI bug + commented script
    Fixed ceiling AI sometime teleporting instead of smoothly transitionning from ground to ceiling

--- Assets/01_Scripts/AI/CeilingAI.cs
+++ Assets/01_Scripts/AI/CeilingAI.cs
@@ -23,57 +23,63 @@ public class CeilingAI : AIInputHandler
 	{
 		base.Awake();
 
+		//Find ground and ceiling mask
 		groundMask = 1 << NavMesh.GetAreaFromName(groundAreaName);
 		ceilingMask = 1 << NavMesh.GetAreaFromName(ceilingAreaName);
 
+		//Set starting agent area mask to ground
 		agent.areaMask = groundMask;
 	}
 
 	protected override void Update()
 	{
+		//If no target, return
 		if (target == null) return;
 
+		//If is not transitionning, do base update
 		if (!isTransitioning)
 			base.Update();
 
+		//Handle the surface switching logic
 		HandleSurfaceLogic();
 	}
 
 	private void HandleSurfaceLogic()
 	{
+		//Check if is transitionning
 		if (isTransitioning)
 		{
+			//Check if is grounded, if yes, stop transitionning
 			if (movementController.IsGroundedLocal())
-			{
 				isTransitioning = false;
-				agent.enabled = true;
-			}
+			//In all cases, return
 			return;
 		}
 
+		//Calculate distance between AI and target
 		float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
+		//If is not on the ceiling, and is far from target and has a ceiling above, then transition to ceiling walk
 		if (!isOnCeiling && distanceToTarget > switchToGroundDistance && HasCeilingAbove())
-		{
 			StartCoroutine(TransitionToCeiling());
-		}
 
+		//If is on ceiling and is close to target, then transition to ground walk
 		if(isOnCeiling && distanceToTarget <= switchToGroundDistance)
-		{
 			StartCoroutine(TransitionToGround());
-		}
 	}
 
 	private bool HasCeilingAbove()
 	{
+		//Raycast up to check for ceiling presence
 		return Physics.Raycast(transform.position, Vector3.up, surfaceCheckDistance);
 	}
 
 	private IEnumerator TransitionToCeiling()
 	{
+		//Start transitionning
 		isTransitioning = true;
-		agent.enabled = false;
 
+		//Check for ceiling, if none found return
 		RaycastHit hit;
 		if(!Physics.Raycast(transform.position, Vector3.up, out hit, 20f))
 		{
@@ -82,74 +88,72 @@ public class CeilingAI : AIInputHandler
 			yield break;
 		}
 
-		Vector3 ceilingPoint = hit.point + hit.normal * .1f;
-		transform.position = ceilingPoint;
-
+		//Flip character then wait for it to be grounded
 		FlipCharacter(true);
+		yield return new WaitUntil(() => movementController.IsGroundedLocal());
 
-		yield return null;
-
+		//Check for ceiling navmesh presence, if found change area mask to ceiling mask and set on ceiling
 		NavMeshHit navHit;
 		if (NavMesh.SamplePosition(transform.position, out navHit, 10f, ceilingMask))
 		{
-			agent.Warp(navHit.position);
 			agent.areaMask = ceilingMask;
-			agent.enabled = true;
 			isOnCeiling = true;
 		}
 		else
-		{
 			Debug.LogWarning("Ceiling collider found but no NavMesh baked there!");
-		}
 
+		//Stop transitionning
 		isTransitioning = false;
 	}
 
 	private IEnumerator TransitionToGround()
 	{
+		//Start transitionning
 		isTransitioning = true;
-		agent.enabled = false;
 
+		//Flip character then wait for it to be grounded
 		FlipCharacter(false);
-
 		yield return new WaitUntil(() => movementController.IsGroundedLocal());
 
+		//Check for ground navmesh presence, if found change area mask to ground mask and set off ceiling
 		NavMeshHit hit;
 		if(NavMesh.SamplePosition(transform.position, out hit, 10f, groundMask))
 		{
-			agent.Warp(hit.position);
 			agent.areaMask = groundMask;
-			agent.enabled = true;
 			isOnCeiling = false;
 		}
 		else
-		{
 			Debug.LogWarning("No ground NavMesh found near position!");
-		}
 
+		//Stop transitionning
 		isTransitioning = false;
 	}
 
 	private void FlipCharacter(bool toCeiling)
 	{
+		//Find forward vector
 		Vector3 forward = transform.forward;
 
+		//Rotate toward the forward direction with the current up depending on wether is on ground or on ceiling
 		if (toCeiling)
 			transform.rotation = Quaternion.LookRotation(forward, -Vector3.up);
 		else
 			transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
 
+		//Reset Y velocity to 0
 		movementController.ResetVerticalVelocity();
 	}
 
 	protected override void MoveTowardsTarget()
 	{
+		//If not grounded, set movement to 0
 		if (!movementController.IsGroundedLocal())
 		{
 			movementController.SetMovementInput(Vector2.zero);
 			return;
 		}
 
+		//Check if agent is enabled and on mesh, if not, return
 		if (!agent.enabled || !agent.isOnNavMesh) return;
 
 		base.MoveTowardsTarget();
--- Assets/01_Scripts/AIInputHandler.cs
+++ Assets/01_Scripts/AIInputHandler.cs
@@ -14,12 +14,15 @@ public class AIInputHandler : MonoBehaviour
 
 	protected virtual void Awake()
 	{
+		//Set character movement if not already set
 		if (movementController == null)
 			movementController = GetComponent<CharacterMovement>();
 
+		//Set agent if not already set
 		if (agent == null)
 			agent = GetComponent<NavMeshAgent>();
 
+		//Turn off the agent handling of position and rotation, so the movement can all be handled by the character movement
 		agent.updatePosition = false;
 		agent.updateRotation = false;
 		agent.updateUpAxis = false;
@@ -27,47 +30,61 @@ public class AIInputHandler : MonoBehaviour
 
 	protected virtual void Update()
 	{
+		//Return if no target found
 		if (target == null) return;
 
+		//Move toward target and face toward movement direction
 		MoveTowardsTarget();
 		FaceMovementDirection();
 	}
 
 	protected virtual void MoveTowardsTarget()
 	{
+		//Set the destination to the target
 		agent.SetDestination(target.position);
 
+		//Check if the remaining distance between AI and target is less than stopping distance, if yes set the movement to 0 and return
 		if(agent.remainingDistance <= stoppingDistance)
 		{
 			movementController.SetMovementInput(Vector2.zero);
 			return;
 		}
 
+		//Get next point to move and it's direction
 		Vector3 nextPoint = agent.steeringTarget;
 		Vector3 direction = (nextPoint - transform.position).normalized;
 
+		//Get the direction from global to local
 		Vector3 localDir = transform.InverseTransformDirection(direction);
 
+		//Set the movement to the desired direction
 		movementController.SetMovementInput(new Vector2(localDir.x, localDir.z));
 
+		//Set next pos to current pos
 		agent.nextPosition = transform.position;
 	}
 
 	protected virtual void FaceMovementDirection()
 	{
-
+		//Check if agent is enabled and on mesh, if not, return
 		if (!agent.enabled || !agent.isOnNavMesh) return;
 
+		//Calculate the movement direction toward next point
 		Vector3 direction = (agent.steeringTarget - transform.position);
 
+		//Check if direction is close to 0, it means we're already on point, if so, return
 		if (direction.sqrMagnitude < .01f) return;
 
+		//Keep rotation aligned with the current surface
 		direction = Vector3.ProjectOnPlane(direction, transform.up);
 
+		//Set target rotation to the movement direction with the AI current local up
 		Quaternion targetRotation = Quaternion.LookRotation(direction, transform.up);
 
+		//Smoothly rotate toward the target rotation
 		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 8f);
 	}
 
+	//Set a new target
 	public void SetTarget(Transform newTarget) => target = newTarget;
 }
